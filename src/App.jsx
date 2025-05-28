64fe33c24d22cf1988db276e1f4e39d53a000729
Gianvito Lovecchio <gianvito1996@gmail.com>
2025-05-28 19:34:03 +0200

    creazione progetto ed inserimento tailwind, mui material, mui icons-material, react-router

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './global.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center my-4">Welcome to My App</h1>
+    </>
+  )
+}
+
+export default App
